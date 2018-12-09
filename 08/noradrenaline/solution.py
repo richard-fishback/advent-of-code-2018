8b59a42beb67707082acc9a7de65055bca9b21ee
Nora Tramm <31042998+noradrenaline@users.noreply.github.com>
2018-12-08 21:28:26 -0500

    Nora Day 8 - removed blahblah

--- 08/noradrenaline/solution.py
+++ 08/noradrenaline/solution.py
@@ -1,30 +1,5 @@
 # oh jeez, this one is trickier.
 
-# start by just defining what a node looks like
-
-class node():
-    id: int
-    parent: int
-    num_pending_children: int
-    nummeta: int
-    children: [] # dict of childid, childstatus
-    status: int
-    start: int
-    end: int
-    metadata: []
-
-    def parse_node(node_id,node_start):
-        # hmm.
-        print("hold up.")
-
-# hold up. What if I took a different approach for this problem:
-# what if I just painted space as containing either definitions or data?
-# what would that look like?
-# loop through the input. record nodeid, start, numchild(remaining), nummeta
-# for each node pending completion.
-# When you find a node with 0 remaining child ids, grab its metadata,
-# pop it off the list of pending nodes, and decrement the numchilds of the 
-# previous node.
 inp = [int(x) for x in open('input.txt').readline().split()]
 test_inp = [int(x) for x in open('sample_input.txt').readline().split()]
 
@@ -36,27 +11,22 @@ def build_nodes(inputlist,start,node_id,parent):
     this_node['meta_num'] = inputlist[start+1]
     this_node['child_num'] = inputlist[start]
     if inputlist[start]==0:
-        #this_node['metadata'] = inputlist[start+2:start+2+this_node['meta_num']]
         this_node['children'] = []
         this_node['end'] = start+2+inputlist[start+1] # this is actually the start of the next one 
         this_node['metadata'] = inputlist[this_node['end']-this_node['meta_num']:this_node['end']]
         return [this_node]
     else:
-        ##this_node['children'] = [x for x in range(node_id+1,node_id+1+inputlist[start])]
         family = [this_node]
         next_start = start+2
         for child in range(inputlist[start]):
             next_node_id = family[-1]['id'] + 1
             descendents = build_nodes(inputlist,next_start,next_node_id,node_id)
             next_start = descendents[0]['end']
-            # list the children
             family.extend(descendents)
         # so now we have the list of this node and all descendents, but we need
         # to fill in the end of this node
         family[0]['end'] = next_start+family[0]['meta_num']
         family[0]['metadata'] = inputlist[family[0]['end']-family[0]['meta_num']:family[0]['end']]
-        # currently wrong
-        # family[0]['metadata'] = inputlist[family[0]['start']+2:family[0]['start']+2+family[0]['end']];
         return family
 
 #breakdown = build_nodes(test_inp,0,0,-1)
@@ -71,9 +41,7 @@ print("part 1: " + str(s))
 for i in range(len(breakdown)):
     breakdown[i]['children'] = [n['id'] for n in breakdown if n['parent'] == breakdown[i]['id']]
 
-# then get the dang values
 # o jeez, here we go again
-
 def eat_children(node_array,i):
     # return the "value" of the ith entry in the node-array
     if node_array[i]['child_num'] == 0:
